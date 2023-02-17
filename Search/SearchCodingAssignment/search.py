d986c7273d9516431941006f0827ee3f4bc9642e
vcss7 <vs.victorsolis@gmail.com>
2023-02-16 17:39:01 -0800

    add is solved function to search.py

--- Search/SearchCodingAssignment/search.py
+++ Search/SearchCodingAssignment/search.py
@@ -4,6 +4,27 @@ LEFT = 1
 DOWN = 2
 RIGHT = 3
 
+def is_solved(puzzle):
+    """
+    Checks if a puzzle is in the solved state:
+        1 2 3
+        4 5 6
+        7 8 0
+
+    Arguments:
+    - puzzle: Node object to check
+
+    Return:
+    is_solved: boolean of whethe the puzzle is solved or not
+    """
+    if puzzle[0][0] != 1 and puzzle[0][1] != 2 and puzzle[0][2] != 3:
+        return false
+    if puzzle[1][0] != 4 and puzzle[1][1] != 5 and puzzle[1][2] != 6:
+        return false
+    if puzzle[2][0] != 7 and puzzle[2][1] != 8 and puzzle[2][2] != 0:
+        return false
+
+
 def BFS(puzzle):
     """
     Breadth-First Search.
