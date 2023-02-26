a6aa2c9087e20b0ce6e8eebb833b7241ca79f913
vcss7 <vs.victorsolis@gmail.com>
2023-02-25 22:40:57 -0800

    format puzzle and shit tile functions

--- Search/SearchCodingAssignment/search.py
+++ Search/SearchCodingAssignment/search.py
@@ -5,15 +5,79 @@ DOWN = 2
 RIGHT = 3
 
 # solved state of puzzle
-GOAL_STATE = [
-]
+GOAL_STATE = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
 
 def format_puzzle(puzzle):
-    pass
+    """
+    Takes a list of list of strings with whitespace as a delimiter and returns
+    a list of list of integers.
+
+    Arguments:
+        puzzle: Node object represeting initial state of the puzzle
+    
+    Return:
+        formatted_puzzle: A list of list of integers representing the puzzle.
+    """
+    formatted_puzzle = []
+
+    for row in puzzle:
+        formatted_row = []
+        for tile in row:
+            formatted_row.append(int(tile))
+        formatted_puzzle.append(formatted_row)
+
+    return formatted_puzzle
 
 
 def shift_tile(puzzle, up=0, left=0, down=0, right=0):
-    pass
+    """
+    Takes a puzzle and shifts the tile in the specified direction.
+    When a direction value is one, the tile is shifted in that direction.
+    Only one direction value can be one at a time.
+
+    Arguments:
+        puzzle: Node object representing initial state of the puzzle
+        up: True if tile should be shifted up, False otherwise
+        left: True if tile should be shifted left, False otherwise
+        down: True if tile should be shifted down, False otherwise
+        right: True if tile should be shifted right, False otherwise
+
+    Return:
+        True if tile was shifted, False otherwise
+    """
+
+    max_row = len(puzzle) - 1
+    max_col = len(puzzle[0]) - 1
+
+    if up + left + down + right == 1:
+
+        # find location of the zero tile
+        for row in ranger(max_row + 1):
+            for col in range(max_col + 1):
+                if puzzle[row][col] == 0:
+                    zero_row = row
+                    zero_col = col
+        
+        # shift tile 
+        if up and zero_row < max_row:
+            puzzle[zero_row][zero_col] = puzzle[zero_row + 1][zero_col]
+            puzzle[zero_row + 1][zero_col] = 0
+            return True
+        elif left and zero_col < max_col:
+            puzzle[zero_row][zero_col] = puzzle[zero_row][zero_col + 1]
+            puzzle[zero_row][zero_col + 1] = 0
+            return True
+        elif down and zero_row > 0:
+            puzzle[zero_row][zero_col] = puzzle[zero_row - 1][zero_col]
+            puzzle[zero_row - 1][zero_col] = 0
+            return True
+        elif right and zero_col > 0:
+            puzzle[zero_row][zero_col] = puzzle[zero_row][zero_col - 1]
+            puzzle[zero_row][zero_col - 1] = 0
+            return True
+
+    return False
 
 
 def BFS(puzzle):
@@ -21,11 +85,13 @@ def BFS(puzzle):
     Breadth-First Search.
 
     Arguments:
-    - puzzle: Node object representing initial state of the puzzle
+        puzzle: Node object representing initial state of the puzzle
 
     Return:
-    final_solution: An ordered list of moves representing the final solution.
+        final_solution: An ordered list of moves representing the final solution.
     """
+
+    formatted_puzzle = format_puzzle(puzzle)
 
     final_solution = []
 
@@ -37,10 +103,10 @@ def DFS(puzzle):
     Depth-First Search.
 
     Arguments:
-    - puzzle: Node object representing initial state of the puzzle
+        puzzle: Node object representing initial state of the puzzle
 
     Return:
-    final_solution: An ordered list of moves representing the final solution.
+        final_solution: An ordered list of moves representing the final solution.
     """
 
     final_solution = []
@@ -53,10 +119,10 @@ def A_Star_H1(puzzle):
     A-Star with Heuristic 1
 
     Arguments:
-    - puzzle: Node object representing initial state of the puzzle
+        puzzle: Node object representing initial state of the puzzle
 
     Return:
-    final_solution: An ordered list of moves representing the final solution.
+        final_solution: An ordered list of moves representing the final solution.
     """
 
     def find_num_tiles_misplaced(puzzle):
@@ -72,10 +138,10 @@ def A_Star_H2(puzzle):
     A-Star with Heauristic 2
 
     Arguments:
-    - puzzle: Node object representing initial state of the puzzle
+        puzzle: Node object representing initial state of the puzzle
 
     Return:
-    final_solution: An ordered list of moves representing the final solution.
+        final_solution: An ordered list of moves representing the final solution.
     """
 
     def find_manhattan_distance(puzzle):
